5a0c2d0ec9e24b9ee7c1dc347f01db3216f1454c
LancerComet <chw644@hotmail.com>
2016-11-27 15:12:19 +0800

    + Remove Expression.

--- src/expressions/expressions/exp.remove.ts
+++ src/expressions/expressions/exp.remove.ts
@@ -1,11 +1,44 @@
+/// <reference path="../../index.d.ts" />
+
+/*
+ * Expression: Remove.
+ * By LancerComet at 15:06, 2016.11.27.
+ * # Carry Your World #
+ */
+
+import { isKeyword } from '../../parser'
+import { EXPRESSION_LIST } from '../'
+import { NumberLiteral, StringLiteral } from '../../parser'
+
+import { errorHandler } from '../../utils'
+
+const EXP_NAME = 'remove'
+
 /**
  * Expression: remove.
- * This function will be called in parser. 
+ * This function will be called in parser.
+ * 
+ * @example
+ *  remove myDiv
  * 
  * @export
  * @param {Token} currentToken
  * @param {Array<Token>} tokens
  */
 export function remove (currentToken: Token, tokens: Array<Token>) {
+  const removeExpression = new Expression(EXP_NAME)
 
+  // Only one argument.
+  // This argument must be a element variable, can be anything expect keyword and expression.
+  const elementVariableArg = tokens.shift()
+  if (!isKeyword(elementVariableArg.value) && EXPRESSION_LIST.indexOf(elementVariableArg.value) < 0) {
+    removeExpression.insertArg(
+      elementVariableArg.type === 'number'
+        ? new NumberLiteral(<number> elementVariableArg.value)
+        : new StringLiteral(<string> elementVariableArg.value)
+    )
+  } else {
+    errorHandler.typeError(`You can't use a keyword or expression as a variable when calling "${EXP_NAME}".`)
+  }
+ 
 }
